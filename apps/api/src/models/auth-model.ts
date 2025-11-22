e1b28e246303347f3ec96752a61d8b2d2c8d22bb
karloCroDev <karlo.webdev@gmail.com>
2025-11-23 00:11:41 +0100

    Refactored all current code in services and models

---
+++ apps/api/src/models/auth-model.ts
@@ -0,0 +1,129 @@
+// External packages
+import { User } from "@prisma/client";
+
+// Config
+import { prisma } from "@/config/prisma";
+
+export async function findUserByEmail(email: string) {
+  return prisma.user.findUnique({
+    where: { email },
+  });
+}
+
+export async function createUser(data: {
+  firstName: User["firstName"];
+  lastName: User["lastName"];
+  fullname: User["fullname"];
+  email: User["email"];
+  password: User["password"];
+  verificationToken: User["verificationToken"];
+  verificationTokenExpiresAt: User["verificationTokenExpiresAt"];
+}) {
+  return prisma.user.create({ data });
+}
+
+export async function updateVerificationData({
+  email,
+  verificationToken,
+  verificationTokenExpiresAt,
+}: {
+  email: User["email"];
+  verificationToken: User["verificationToken"];
+  verificationTokenExpiresAt: User["verificationTokenExpiresAt"];
+}) {
+  return prisma.user.update({
+    where: { email },
+    data: {
+      verificationToken,
+      verificationTokenExpiresAt,
+    },
+  });
+}
+
+// Verifications
+export async function updateResetPasswordToken({
+  email,
+  resetToken,
+  resetTokenExpireDate,
+}: {
+  email: User["email"];
+  resetToken: User["resetToken"];
+  resetTokenExpireDate: User["resetTokenExpireDate"];
+}) {
+  return prisma.user.update({
+    where: { email },
+    data: {
+      resetToken,
+      resetTokenExpireDate,
+    },
+  });
+}
+
+export async function resetPasswordByToken({
+  expireDate,
+  hashedPassword,
+  resetToken,
+}: {
+  resetToken: NonNullable<User["resetToken"]>;
+  expireDate: bigint;
+  hashedPassword: string;
+}) {
+  return prisma.user.update({
+    where: {
+      resetToken,
+      resetTokenExpireDate: {
+        gt: expireDate,
+      },
+    },
+    data: {
+      password: hashedPassword,
+      resetToken: null,
+      resetTokenExpireDate: null,
+    },
+  });
+}
+
+export async function findUserForOtpVerification(email: string) {
+  return prisma.user.findFirst({
+    where: {
+      email,
+      verificationTokenExpiresAt: {
+        gt: BigInt(Date.now()),
+      },
+    },
+  });
+}
+
+export async function clearOtpVerification(userId: string) {
+  return prisma.user.update({
+    where: { id: userId },
+    data: {
+      verificationToken: null,
+      verificationTokenExpiresAt: null,
+    },
+  });
+}
+
+export async function updateVerificationToken({
+  email,
+  expireDate,
+  hashedOtp,
+}: {
+  email: User["email"];
+  hashedOtp: User["verificationToken"];
+  expireDate: User["verificationTokenExpiresAt"];
+}) {
+  return prisma.user.updateMany({
+    where: {
+      email,
+      AND: [
+        { verificationToken: { not: null } },
+        { verificationToken: { not: "" } },
+      ],
+    },
+    data: {
+      verificationToken: hashedOtp,
+      verificationTokenExpiresAt: expireDate,
+    },
+  });
+}
---
+++ apps/api/src/services/session.service.ts
@@ -0,0 +1,88 @@
+// External packages
+import { Response } from "express";
+
+// Models
+import {
+  finishOnboarding,
+  updateUserOnboarding,
+} from "@/models/onboarding.model";
+
+// Local packaes
+import {
+  AdditionalFormArgs,
+  additionalInformationSchema,
+} from "@repo/schemas/onboarding";
+
+// Lib
+import { generateTokenAndSetCookie } from "@/lib/set-token-cookie";
+
+export async function additionalInformationService(
+  rawData: unknown,
+  userId: string
+) {
+  const { success, data } = additionalInformationSchema.safeParse(rawData);
+
+  if (!success) {
+    return {
+      status: 400,
+      body: { message: "Invalid data provided" },
+    };
+  }
+
+  const payload: Partial<Omit<AdditionalFormArgs, "image">> = {};
+
+  if (data.image) {
+    // S3 logic here in future
+  }
+
+  if (data.bio) payload.bio = data.bio;
+  if (data.DOB) payload.DOB = data.DOB;
+
+  const user = await updateUserOnboarding({ data: payload, userId });
+
+  // Add this in controller
+  //   generateTokenAndSetCookie({
+  //     res,
+  //     userId: user.id,
+  //     role: user.role,
+  //     onboardingFinished: true,
+  //   });
+
+  return {
+    user,
+    status: 200,
+    body: {
+      title: "Account created",
+      message: "Additional information saved successfully",
+    },
+  };
+}
+
+export async function skipAdditionalInformationService(userId: string) {
+  const user = await finishOnboarding(userId);
+
+  if (!user) {
+    return {
+      status: 400,
+      body: {
+        message: "There was an issue finishing onboarding (user missing)",
+      },
+    };
+  }
+
+  // Add this in controller
+  //   generateTokenAndSetCookie({
+  //     res,
+  //     userId: user.id,
+  //     role: user.role,
+  //     onboardingFinished: true,
+  //   });
+
+  return {
+    status: 200,
+    body: {
+      title: "Account created",
+      message: "Onboarding completed successfully",
+    },
+  };
+}
