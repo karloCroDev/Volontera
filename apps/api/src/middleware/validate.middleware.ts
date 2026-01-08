e0787a8000eed6be4080b672f4f2ae0b09bab538
karloCroDev <karlo.webdev@gmail.com>
2026-01-08 19:29:33 +0100

    Added expplanation on frontend for some behavior

--- apps/api/src/middleware/validate.middleware.ts
+++ apps/api/src/middleware/validate.middleware.ts
@@ -17,6 +17,8 @@ export function validate<T>({
 
     if (!result.success) {
       //   console.log("Validation result:", zodErrorDetecter(result.error));  Koristiti za debugiranje kod korisništejna controllera
+
+      // Ovdje samo handleam posebno slučaj responsea od middleewarea, jer može biti bitan za validaciju na frontendu. Iako handleam validaciju formi na frontendu, može se kod npr. get requesta desiti npr. da korisnik ručno mijenja query parametre u URL-u pa da se pošalje nevalidan request. Npr. ako očekujem cuid a korisnik pošalje neki string koji nije validan cuid onda će ovo uhvatiti (svkako će), ali će i frontend moći pravilno obraditi error jer će dobiti odgovor koji očekuje.
       switch (responseOutput) {
         case "server":
           return res
